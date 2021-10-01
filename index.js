f2fbbe96ede0bdc9f4933b54101239ebd4e4ce5a
Davidoff2103 <davidoff.d777@gmail.com>
2021-10-01 17:15:45 +0200

    Añadiendo y ordenando textos.

---
+++ index.js
@@ -0,0 +1,42 @@
+$(function () {
+    $('#cuadrado1').hover(function () {
+        $('#texto1').css('visibility', 'visible');
+    }, function () {
+        $('#texto1').css('visibility', 'hidden');
+    });
+});
+$(function () {
+    $('#cuadrado2').hover(function () {
+        $('#texto2').css('visibility', 'visible');
+    }, function () {
+        $('#texto2').css('visibility', 'hidden');
+    });
+});
+$(function () {
+    $('#cuadrado3').hover(function () {
+        $('#texto3').css('visibility', 'visible');
+    }, function () {
+        $('#texto3').css('visibility', 'hidden');
+    });
+});
+$(function () {
+    $('#cuadrado4').hover(function () {
+        $('#texto4').css('visibility', 'visible');
+    }, function () {
+        $('#texto4').css('visibility', 'hidden');
+    });
+});
+$(function () {
+    $('#cuadrado5').hover(function () {
+        $('#texto5').css('visibility', 'visible');
+    }, function () {
+        $('#texto5').css('visibility', 'hidden');
+    });
+});
+$(function () {
+    $('#cuadrado6').hover(function () {
+        $('#texto6').css('visibility', 'visible');
+    }, function () {
+        $('#texto6').css('visibility', 'hidden');
+    });
+});
